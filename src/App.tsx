2d2c16dbd88c4a0588784d98a48ced9e3fd6de6e
Oleg Soshko <ososhko@MACBOOK-PRO-SOSKO.local>
2023-11-29 09:55:36 +0300

    удалил всё лишнее

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
 import { useState } from 'react';
-import styled from 'styled-components';
-import './App.css';
-
-import { TableLoadOnScroll } from './table/example1';
-import { TableLoadOnScrollSpinner } from './table/example2';
-import { TableLoadOnScrollSkeleton } from './table/example3';
-
 import { OverflowMenu, MenuItem } from '@admiral-ds/react-ui';
 
-// Импорт иконки как URL ресурс. Это дефолтное поведене для vite (https://vitejs.dev/guide/assets.html#importing-asset-as-url)
-import reactLogo from './assets/react.svg';
-
-// Импорт оптимизированной иконки через настроенный SVGR лоадер (https://react-svgr.com/docs/what-is-svgr/)
-import ArrowRightOutline from '@admiral-ds/icons/build/category/CreatePullRequestSolid.svg?react';
-
-// Импорт оригинала иконки через настроенный SVGR лоадер (https://react-svgr.com/docs/what-is-svgr/)
-import BusSolid from '@admiral-ds/icons/public/icons/category/Bus Solid.svg?react';
-
-// Импорт иконки как готового реакт компонента (лоадер не требуется)
-import { CategoryBusOutline } from '@admiral-ds/icons';
-
-import { T, Link } from '@admiral-ds/react-ui';
-
-const Divider = styled.div`
-  width: 10px;
-  height: 12px;
-`;
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const [state, setState] = useState(false);
 
   const t = [
@@ -75,32 +49,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* импорт иконки через директорию assets. Доступ к этой директории есть и у index.html */}
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button className="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <T font="Subtitle/Subtitle 1" as="p">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </T>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <Link appearance="primary" href="https://admiralds.github.io/react-ui" target="_blank" rel="noopener noreferrer">
-        Admiral Storybook
-        <Divider />
-        <ArrowRightOutline width={20} />
-        <CategoryBusOutline width={20} />
-        <BusSolid width={20} />
-      </Link>
       <div className="tables">
         <OverflowMenu
           items={t}
--- src/table/SkeletonComponent.tsx
+++
@@ -1,7 +0,0 @@
-import { skeletonAnimationMixin, PseudoText } from '@admiral-ds/react-ui';
-import styled from 'styled-components';
-
-export const SkeletonComponent = styled(PseudoText)`
-  width: 100%;
-  ${skeletonAnimationMixin}
-`;
